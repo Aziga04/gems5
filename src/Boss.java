0dd3456502f6039798dc1ba4c393eac53e8a9a70
Aziga04 <azizmusaev33@gmail.com>
2022-09-14 20:54:49 +0600

    Initial commit

---
+++ src/Boss.java
@@ -0,0 +1,28 @@
+public class Boss {
+    int health;
+    int damage;
+    int defence;
+
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int i){
+        this.health = health;
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+    public void setDamage(int i){
+        this.damage = damage;
+    }
+
+    public int getDefence(){
+        return defence;
+    }
+    public void setDefence(int i){
+        this.defence = defence;
+
+    }
+}
